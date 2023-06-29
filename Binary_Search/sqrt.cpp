e775399fe09fb6dee7e8ff8e60fdab61e5e38455
Tanya Negi <131293785+TanyaNegi10@users.noreply.github.com>
2023-06-30 00:38:00 +0530

    Add files via upload

---
+++ Binary_Search/sqrt.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int sqrt(int n)
+{
+    int st=0;
+    int e=n-1;
+    while(st<=e)
+    {
+        int mid=(st+e)/2;
+        if(mid*mid==n)
+        {
+            return mid;
+        }
+        else if(mid*mid<n)
+        {
+            st=mid+1;
+        }else{
+            e=mid-1;
+        }
+    }
+    return -1;
+}
+int main()
+{
+    int n=49;
+    int ans=sqrt(n);
+    cout<<ans;
+
+}
