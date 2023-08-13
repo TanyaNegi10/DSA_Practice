13084dbd089f236d2f40dd943bf3178333db277f
Tanya Negi <131293785+TanyaNegi10@users.noreply.github.com>
2023-08-13 23:27:46 +0530

    Update armstrong_num.cpp

--- cpp/armstrong_num.cpp
+++ cpp/armstrong_num.cpp
@@ -1,1 +1,37 @@
 #include<bits/stdc++.h>
+using namespace std;
+bool armstrong(int n)
+{
+    int duplicate=n;
+    int original=n;
+    int count=0;
+    int sum=0;
+    while(duplicate!=0)
+    {
+        duplicate=duplicate/10;
+        count++;
+    }
+    while(n!=0)
+    {
+        int r=n%10;
+        sum=sum+pow(r,count);
+        n=n/10;
+    }
+    if(original==sum)
+    {
+        return true;
+    }
+return false;
+
+}
+int main()
+{
+    int n=1633;
+    if(armstrong(n))
+    {
+        cout<<"Armstrong number";
+    }else{
+        cout<<"Not an armstrong number";
+    }
+    return 0;
+}
