f1ef146b5d9b1ce2c0fcecb277cc3fbde6318985
Tanya Negi <131293785+TanyaNegi10@users.noreply.github.com>
2023-09-02 12:15:08 +0530

    Add files via upload

---
+++ BitManipulation/CheckEvenOdd.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n=6;
+    if(n & 1)
+    {
+        cout<<"Number is odd";
+    }else{
+        cout<<"Number is even";
+    }
+    return 0;
+}
