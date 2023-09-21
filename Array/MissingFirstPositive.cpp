96c480a19615adce93be2123ae61b2d155e88fad
Tanya Negi <131293785+TanyaNegi10@users.noreply.github.com>
2023-09-21 22:11:48 +0530

    Update MissingFirstPositive.cpp

--- Array/MissingFirstPositive.cpp
+++ Array/MissingFirstPositive.cpp
@@ -1,37 +1,65 @@
 // Brute force
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 int main()
 {
-    vector<int> nums = {3, 4, -1, 1};
-    int n = nums.size();
-    int ans = 1;
-    int i = 0;
-    while (i < n)
+    vector<int>nums={3, 4, -1, 1};
+    int n=nums.size();
+    set<int>s;
+    for(int i=0;i<n;i++)
     {
-        if (nums[i] <= 0)
+        s.insert(nums[i]);
+    }
+    int i=1;
+    while(i<=n)
+    {
+        if(s.find(i)!=s.end())
         {
             i++;
-        }
-        int ci = nums[i] - 1;
-
-        if (i != ci && nums[i] <= n && nums[ci]!=ci+1)
-        {
-            swap(nums[i], nums[ci]);
-        }
-        else
-        {
-            i++;
+        }else{
+            cout<<i<<" ";
+            break;
         }
     }
 
-    for (int k = 0; k < n; k++)
-    {
-        if (nums[k] != k + 1)
-        {
-            ans = k + 1;
-        }
-    }
-    cout << ans;
     return 0;
 }
+
+//Optimal Approach
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     vector<int> nums = {3, 4, -1, 1};
+//     int n = nums.size();
+//     int ans = 1;
+//     int i = 0;
+//     while (i < n)
+//     {
+//         if (nums[i] <= 0)
+//         {
+//             i++;
+//         }
+//         int ci = nums[i] - 1;
+
+//         if (i != ci && nums[i] <= n && nums[ci]!=ci+1)
+//         {
+//             swap(nums[i], nums[ci]);
+//         }
+//         else
+//         {
+//             i++;
+//         }
+//     }
+
+//     for (int k = 0; k < n; k++)
+//     {
+//         if (nums[k] != k + 1)
+//         {
+//             ans = k + 1;
+//         }
+//     }
+//     cout << ans;
+//     return 0;
+// }
